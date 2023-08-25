9e17b7916a69d3443e000a7791f7f784992785b6
Dr. Raed <drraed.jb@gmail.com>
2023-08-25 14:05:22 +0200

    tutorial for dio

---
+++ lib/modelphoto/modelforphoto.dart
@@ -0,0 +1,12 @@
+class PhotoDart{
+  final int albumId;
+  final int id;
+  final String title;
+  final String url;
+  final String thumbnailUrl;
+  PhotoDart({required this.albumId,required this.id, required this. title,required this.url,required this.thumbnailUrl});
+
+  
+}
+
+
